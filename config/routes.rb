0710d901d44fc299bf5858ecff094405164f6c82
Ashi Krishnan <me@ashi.works>
2015-12-09 02:04:25 -0500

    Cookies and sessions solution.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'products#index'
+  post '/' => 'products#add'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
