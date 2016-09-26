c46da390f6ed1d9399c6ca6f0689953b5e9d55ba
mendelB <mendelblesofsky@gmail.com>
2016-09-27 01:59:12 +0300

    change cart method and process of displaying cart to align with readme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :cart
 
   def cart
-    @cart = (session[:cart] ||= [])
+    session[:cart] ||= []
   end
 end
