85db1cb68246e60e6463d82ba2c0753dc3181970
Johann Kerr <johannkerr@gmail.com>
2017-12-18 17:36:45 -0500

    updating Gemfile

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module CookiesAndSessionsLab
   class Application < Rails::Application
+    config.web_console.development_only = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
